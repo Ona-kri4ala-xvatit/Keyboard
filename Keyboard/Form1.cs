cc86220d8ba5bcdcfcbabddd075029172587123b
Nijat Muradov <live4ever4@gmail.com>
2023-05-26 16:25:00 +0400

    Edit code

--- Keyboard/Form1.cs
+++ Keyboard/Form1.cs
@@ -9,12 +9,7 @@ namespace Keyboard
 
         private void ButtonClick(object sender, EventArgs e)
         {
-            //richTextBox.Text += (sender as string[]).ToArray();
-                RichTextBox richTextBox2 = sender as RichTextBox;
-            richTextBox.Text = richTextBox2.Text;
-            //if (sender is RichTextBox rtxt)
-            //{
-            //}
+            
         }
     }
 }
